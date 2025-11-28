80238d386de8504f69b17771622c577032294489
Andrii Kanava <andrii.kanava@student.nhlstenden.com>
2025-11-28 12:58:45 +0100

    add links to banner

--- portfolio/src/components/welcome-banner/banner.jsx
+++ portfolio/src/components/welcome-banner/banner.jsx
@@ -9,6 +9,10 @@ function Banner() {
         <div className="banner-container">
             <h1>Welcome{user ? `, ${user.username}` : ""} 👋</h1>
             <h3>Andrii's Portfolio</h3>
+            <div className="links">
+                <a href="https://github.com/andriikanava">Github</a>
+                <a href="mailto:andrii.kanava@student.nhlstenden.com">Contact</a>
+            </div>
         </div>
     );
 }
